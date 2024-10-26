073bb457d99d2fec8146c0fe4fae3efee360e744
vansh.shah <vanshshah174@gmail.com>
2024-10-26 02:33:58 -0400

    Updates done

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+  <View className="flex-1 items-center justify-center bg-white"> 
+    <Text>Home Screen</Text>
+    <StatusBar style='auto' />
+    <Link href="/profile">Go to</Link> 
+  </View>
+  );
+}
+
+
