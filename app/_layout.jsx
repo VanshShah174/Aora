7004a036de33a6e840e02eacd4e50032b848bdaa
vansh.shah <vanshshah174@gmail.com>
2024-10-26 02:45:57 -0400

    Required Changes Done

---
+++ app/_layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+    <Text>Aora!</Text>
+    
+    <StatusBar style="auto" />
+  </View>
+  );
+};
+
+
+
+export default App;
