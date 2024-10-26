ec95fd6d90d97c1b9d539e424751195379ed470b
vansh.shah <vanshshah174@gmail.com>
2024-10-26 03:19:51 -0400

    Required Images and Fonts Added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Slot, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+      });
+
+      useEffect(() => {
+        if(error) throw error;
+
+        if(fontsLoaded) SplashScreen.hideAsync();
+      },[fontsLoaded, error])
+
+      if(!fontsLoaded && error) return null;
 
     return(
         <Stack>
@@ -10,6 +33,18 @@ const RootLayout = () => {
         </Stack>
     )
 
+
+// function hideAsync(): Promise<boolean>
+// Hides the native splash screen immediately. 
+// Be careful to ensure that your app has content ready to display when you hide the splash screen,
+//  or you may see a blank screen briefly. See the "Usage" section for an example
+
+
+// Makes the native splash screen (configured in app.json) remain visible until hideAsync is called.
+
+// Important note: It is recommended to call this in global scope without awaiting,
+//  rather than inside React components or hooks, because otherwise this might be called too late,
+//   when the splash screen is already hidden.
 
 //   return (
 //     <>
--- app/index.jsx
+++ app/index.jsx
@@ -4,14 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 
 
-const App = () => {
+export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-    <Text className="text-3xl">Aora!</Text>
+    <Text className="text-3xl font-pblack">Aora!</Text>
     <StatusBar style="auto" />
     <Link href="/profile" style={{ color:"blue"}}>Go to Profile</Link>
   </View>
   );
 };
 
-export default App;
